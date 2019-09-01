278afc533d8338072e20820794c93bd26b8b6310
Sid Kandasamy <sidkandasamy@outlook.com>
2019-09-01 18:16:59 +0100

    finished css and updated javascript

---
+++ script.js
@@ -0,0 +1,57 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+let shuffledQuestion, currentQuestionIndex  
+
+startButton.addEventListener('click',startGame)
+
+function startGame(){
+    startButton.classList.add('hide')   
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide') 
+    setNextQuestion()
+}
+
+function setNextQuestion(){
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function selectAnswer(){
+
+}
+
+const questions = [
+    {
+      question: 'What is 10 + 10?',
+      answers: [
+        { text: '20', correct: true },
+        { text: '80', correct: false }
+      ]
+    },
+    {
+      question: 'What is 25 * 25',
+      answers: [
+        { text: '90', correct: false },
+        { text: '27', correct: false },
+        { text: '625', correct: true },
+        { text: '89', correct: false }
+      ]
+    },
+    {
+      question: '87 + (10*34)',
+      answers: [
+        { text: '500', correct: false },
+        { text: '427', correct: true },
+        { text: '150', correct: false },
+        { text: '78', correct: false }
+      ]
+    },
+    {
+      question: '82 + 18',
+      answers: [
+        { text: '100', correct: false },
+        { text: '8', correct: true }
+      ]
+    }
+  ]
