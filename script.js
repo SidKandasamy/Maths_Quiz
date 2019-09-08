a2c5216aa63034f167da22a9b8e5f44815a11327
Sid Kandasamy <sidkandasamy@outlook.com>
2019-09-08 10:38:02 +0100

    javascript qestions completed

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
-let shuffledQuestion, currentQuestionIndex  
+let shuffledQuestion, currentQuestionIndex  //default both values to undefined
+const questionElement = document.getElementById('question-contrainer')
+const answerButtonsElement = docuemnt.getElementById('answer-buttons')
 
 startButton.addEventListener('click',startGame)
 
 function startGame(){
     startButton.classList.add('hide')   
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
+    shuffledQuestions = questions.sort(() => Math.random() - .5) //takes curreunt arry and sorts in + and negative way. Maths.random shuffles our qs randomly
+    currentQuestionIndex = 0 //start on first q on our shuffled array
     questionContainerElement.classList.remove('hide') 
     setNextQuestion()
 }
@@ -15,6 +17,11 @@ function startGame(){
 function setNextQuestion(){
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+    questionElement.innerText = question.question
+
 }
 
 function selectAnswer(){
@@ -51,7 +58,9 @@ const questions = [
       question: '82 + 18',
       answers: [
         { text: '100', correct: false },
-        { text: '8', correct: true }
+        { text: '8', correct: true },
+        { text: '150', correct: false },
+        { text: '78', correct: false }
       ]
     }
   ]
